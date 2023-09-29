58a6a5efaecd9d633eb810718690073a9b8c6e62
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-09-29 17:51:26 -0300

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import sessionMiddleware from './middlewares/session.js';
 import setRouters from './routes/index.js';
 import setSockets from './sockets/index.js';
 import { DaoConnector, daoManagersMiddleware } from './dao/connector.js';
+import consoleActivity from './middlewares/console.js';
 
 // Instantiate the express application:
 const app = express();
@@ -24,6 +26,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static(process.cwd() + '/public'));
+app.use(sessionMiddleware);
+app.use(consoleActivity({ ip: false, color: true, body: true }));
 
 // Using routers
 setRouters(app);
