45070750e6758f33ecdc4915ccc5f90f68c52493
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-10-23 16:57:08 -0300

    user attributes

--- src/dao/mongo/models/user.js
+++ src/dao/mongo/models/user.js
@@ -5,11 +5,16 @@ const userSchema = new mongoose.Schema({
     first_name: String,
     last_name: String,
     email: String,
+    age: Number,
     password: String,
-    rol: {
+    role: {
         type: String,
         enum: ['admin', 'user'],
         default: 'user',
+    },
+    cart: {
+        type: mongoose.Types.ObjectId,
+        ref: 'carts',
     },
 });
 
