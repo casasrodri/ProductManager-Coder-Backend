8352e996a66f0045e3763dd6ac24f9b82f2ccc44
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-11-13 18:34:00 -0300

    jwt lee el usuario

--- src/middlewares/jwt.js
+++
@@ -1,7 +0,0 @@
-import { userRepository } from '../repositories/index.js';
-
-export const loadUser = async (req, res, next) => {
-    const { userId } = req.user;
-    req.user = await userRepository.getById(userId).populate('cart').lean();
-    next();
-};
