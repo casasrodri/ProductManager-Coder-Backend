7f2254be975e693a31f258c63cf3f3ca015c8bcb
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-11-21 23:14:15 -0300

    winston config

---
+++ src/utils/logger.js
@@ -0,0 +1,35 @@
+import winston from "winston";
+import { config } from "dotenv";
+config();
+
+let envTransports
+
+const customLevels = {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5,
+};
+
+switch (process.env.ENVIRONMENT) {
+    case 'development':
+        envTransports = [
+            new winston.transports.Console({ level: 'debug' }),
+        ]
+        break;
+    case 'production':
+        envTransports = [
+            new winston.transports.Console({ level: 'info' }),
+            new winston.transports.File({ name: 'errors-file', filename: 'logs/errors.log', level: 'error' }),
+        ]
+        break;
+}
+
+export default winston.createLogger({
+    transports: envTransports,
+    levels: customLevels,
+});
+
+
