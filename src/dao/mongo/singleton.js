e0d768c2f71192e1cbc7303a97b6a360c53f714f
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-12-21 01:46:56 -0300

    mongo connect

--- src/dao/mongo/singleton.js
+++ src/dao/mongo/singleton.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 import config from '../../config/config.js';
 import logger from '../../utils/logger.js';
 
+export const connectMongo = () => mongoose.connect(config.mongoUri, {});
+
 export default class MongoSingleton {
     static #instance;
 
     constructor() {
-        mongoose.connect(config.mongoUri, {});
+        connectMongo();
         logger.info('Ⓜ️  Connection to MongoDB established successfully');
     }
 
