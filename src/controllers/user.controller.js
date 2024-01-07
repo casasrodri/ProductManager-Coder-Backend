7a362055bd1bed9d1a40343b87183476d90c5357
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-01-06 21:17:33 -0300

    endpoint upload documents

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { userRepository } from '../repositories/index.js';
+import multer from 'multer';
 
 export default class UserController {
 
@@ -20,5 +21,57 @@ export default class UserController {
         user.save();
 
         res.json({ status: 'ok', message: 'User updated', data: { email: user.email, newRole } })
+    }
+
+    async uploadDocuments(req, res) {
+        const { uid } = req.params;
+        const { files } = req;
+
+        // Filter valid files
+        const validFiles = {}
+
+        for (const file of files) {
+            if (validFiles[file.fieldname]) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: `Duplicated fieldname: ${file.fieldname}`,
+                    data: { fieldname: file.fieldname },
+                });
+            }
+
+            if (['identification', 'address', 'accountStatus'].includes(file.fieldname)) {
+                validFiles[file.fieldname] = {
+                    originalname: file.originalname,
+                    filename: file.filename,
+                    path: file.path,
+                    size: file.size,
+                    mimetype: file.mimetype,
+                }
+            }
+        }
+
+        // Get user
+        const user = await userRepository.getById(uid);
+
+        if (!user)
+            return res.status(401).json({
+                status: 'error',
+                message: 'User not found.',
+                data: { uid },
+            });
+
+        // Save documents
+        user.documents = []
+        for (const [documentType, documentProps] of Object.entries(validFiles)) {
+            user.documents.push(
+                {
+                    name: documentType,
+                    reference: documentProps.path
+                }
+            )
+        }
+        user.save();
+
+        res.json({ status: 'ok', message: 'Documents saved.', data: { documents: Object.keys(validFiles) } })
     }
 }
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,8 +1,15 @@
 import { Router } from '../services/errors/customRouter.js';
 import { userController } from '../controllers/index.js';
+import { documentsUploader } from '../middlewares/multer.js';
 
 const router = Router();
 
 router.get('/premium/:uid', userController.premiumSwith);
+
+router.post(
+    '/:uid/documents',
+    documentsUploader.any(),
+    userController.uploadDocuments
+);
 
 export default router;
