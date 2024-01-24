32987751acbf160ef0572b30751b8259c6351bd0
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-01-24 00:50:28 -0300

    endpoints reorder

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -7,15 +7,15 @@ router.post('/', cartController.createCart);
 
 router.get('/:cid', cartController.getCartById);
 
-router.post('/:cid/product/:pid', cartController.addProductToCartId);
-
 router.put('/:cid', cartController.updateProducts);
+
+router.delete('/:cid', cartController.clearCartById);
+
+router.post('/:cid/product/:pid', cartController.addProductToCartId);
 
 router.put('/:cid/product/:pid', cartController.updateProductQuantity);
 
 router.delete('/:cid/product/:pid', cartController.removeProductFromCartId);
-
-router.delete('/:cid', cartController.clearCartById);
 
 router.post('/:cid/purchase', cartController.purchaseCartById);
 
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,18 +7,18 @@ const router = Router();
 
 router.get('/', productController.getProductsPaginate);
 
+router.post('/', getBodyProduct, productController.addProduct);
+
 router.get('/:pid', productController.getProductById);
 
-router.post('/', getBodyProduct, productController.addProduct);
+router.put('/:pid', getBodyProduct, productController.updateProductById);
+
+router.delete('/:pid', productController.deleteProductById);
 
 router.post(
     '/:pid/thumbnails',
     thumbnailsUploader.array('thumbnails'),
     productController.addThumbnail
 );
-
-router.put('/:pid', getBodyProduct, productController.updateProductById);
-
-router.delete('/:pid', productController.deleteProductById);
 
 export default router;
